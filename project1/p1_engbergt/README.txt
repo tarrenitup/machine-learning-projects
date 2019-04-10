0f932cd3e649a80821bc61624ba5e0bd65a9f4ab
Tarren Engberg <tarren@outlook.com>
2019-04-10 12:31:05 -0700

    first commit

---
+++ project1/p1_engbergt/README.txt
@@ -0,0 +1,6 @@
+Directions: https://www.tensorflow.org/install/pip
+
+Activate the environment:
+source ./venv/bin/activate
+
+
---
+++ project1/p1_engbergt/q1_2.py
@@ -0,0 +1,15 @@
+# 1) import Boston housing training data.
+
+# load training data into corresponding X (features) & Y (desired outputs) matrices.
+
+# add as first column of X the dummy variable of '1'.
+
+# compute optimal weight vector w (may use package such as numpy).
+
+# print the learned weight vector (w).
+
+# 2) import Boston housing testing data.
+
+# using w, compute the ASE of training and testing data.
+
+# print out trainging ASE and testing ASE and which is largest.
