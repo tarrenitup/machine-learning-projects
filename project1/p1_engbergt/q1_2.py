9de47655fe0fdbcb9a7931532d784e1bd621b734
Tarren Engberg <tarren@outlook.com>
2019-04-12 14:03:43 -0700

    data in matrices.

--- project1/p1_engbergt/q1_2.py
+++ project1/p1_engbergt/q1_2.py
@@ -1,15 +1,54 @@
+import numpy as np
+# Project 1 - Tarren Engberg (engbergt), sole group member.
+
 # 1) import Boston housing training data.
 
+def fileTo2dFloatArr(path):
+    return map(lambda line : map(lambda str: float(str), line.split()), open(path, 'r').read().splitlines())
+
+training2dArr = fileTo2dFloatArr('data/housing_train.txt')
+
+###
 # load training data into corresponding X (features) & Y (desired outputs) matrices.
+###
 
+# Matrix (2D array) of features.
+
+def removeLast(lst):
+    newLst = []
+    i = 0
+    while i < len(lst) - 1:
+        newLst.append(lst[i])
+        i += 1
+    return newLst
+
+X = map(removeLast, training2dArr)
+
+# Matirx of desired outputs.
+
+Y = map(lambda line : line[len(line) - 1], training2dArr)
+
+
+###
 # add as first column of X the dummy variable of '1'.
+###
 
+###
 # compute optimal weight vector w (may use package such as numpy).
+###
 
+###
 # print the learned weight vector (w).
+###
 
+###
 # 2) import Boston housing testing data.
+###
 
+###
 # using w, compute the ASE of training and testing data.
+###
 
+###
 # print out trainging ASE and testing ASE and which is largest.
+###
