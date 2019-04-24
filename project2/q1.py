34e75a221d5a5d0260b0dcdbd6c28431a5866572
Tarren Engberg <tarren@outlook.com>
2019-04-24 09:50:44 -0700

    k nearest working.

--- project2/q1.py
+++ project2/q1.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 # load data
 knnDummyData = pd.read_csv('data/knn_dummy.csv', header=None)
-knnTrainData = pd.read_csv('data/knn_train.csv', header=None)
-knnTestData = pd.read_csv('data/knn_test.csv', header=None)
+# knnTrainData = pd.read_csv('data/knn_train.csv', header=None)
+# knnTestData = pd.read_csv('data/knn_test.csv', header=None)
 
 
 
@@ -37,43 +37,25 @@ def distanceBetweenPoints(pointOneArray, pointTwoArray):
         index += 1
     return np.sqrt(total)
 
-def findNearestIndex(point, points):
+def findNearestIndex(point, points, ignoreIndices):
     bestIndex = 0
+    while(bestIndex in ignoreIndices):
+        bestIndex += 1
     index = 0
     for p in points:
-        thisDistance = distanceBetweenPoints(p, point)
-        bestDistanceSoFar = distanceBetweenPoints(points[bestIndex], point)
-        if (thisDistance < bestDistanceSoFar):
-            bestIndex = index
+        if (not index in ignoreIndices):
+            thisDistance = distanceBetweenPoints(p, point)
+            bestDistanceSoFar = distanceBetweenPoints(points[bestIndex], point)
+            if (thisDistance < bestDistanceSoFar):
+                bestIndex = index
         index += 1
     return bestIndex
 
-# print(tryB)
-# first = findNearestIndex(tryA, tryB)
-# tryB = np.delete(tryB, first, axis=0)
-# print(tryB)
-# # second = findNearestIndex(tryA, tryB)
-# tryB = np.delete(tryB, second)
-# print(tryB)
-
-
 def findKNearest(k, point, points):
-    bestIndices = np.array([])
+    bestIndices = []
     for i in range(k):
-        bestIndices = np.append(bestIndices, findNearestIndex(point, points))
-        points = np.delete(points, bestIndices[-1], axis=0)
+        nearestPointIndex = findNearestIndex(point, points, bestIndices)
+        bestIndices.extend([nearestPointIndex])
     return bestIndices
 
-print( findKNearest(3, tryA, tryB) )
-
-
-
-# print(tryA)
-# tryA = np.delete(tryA, 0)
-# print(tryA)
-# tryA = np.delete(tryA, 0)
-# print(tryA)
-
-# print(tryA)
-# tryA = np.append(tryA, 7)
-# print(tryA)
+print(findKNearest(2, tryA, tryB))
