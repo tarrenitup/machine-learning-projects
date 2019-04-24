5b734d8998ba8bf5fb0bbd94dce816409d2cea2a
Tarren Engberg <tarren@outlook.com>
2019-04-24 10:37:38 -0700

    voting works.

--- project2/q1.py
+++ project2/q1.py
@@ -3,29 +3,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
 # load data
-knnDummyData = pd.read_csv('data/knn_dummy.csv', header=None)
+knnDummyTrainData = pd.read_csv('data/knn_dummy_train.csv', header=None)
+knnDummyTestData = pd.read_csv('data/knn_dummy_test.csv', header=None)
 # knnTrainData = pd.read_csv('data/knn_train.csv', header=None)
 # knnTestData = pd.read_csv('data/knn_test.csv', header=None)
-
 
 
 # separate data into labels and features
 tryA = np.array([1, 2, 3])
 tryB = np.array([[2, 3, 4], 
-        [1.12, 20, 3], 
-        [9, 10, 11]])
+                [1.12, 20, 3], 
+                [9, 10, 11]])
+tryL = np.array([1, 1, 1])
 
-knnDummyLabels = knnDummyData.iloc[:, :1].values
-knnDummyFeatures = knnDummyData.iloc[:, 1:].values 
+knnDummyTrainLabels = knnDummyTrainData.iloc[:, :1].values
+knnDummyTrainFeatures = knnDummyTrainData.iloc[:, 1:].values
+
+knnDummyTestLabels = knnDummyTestData.iloc[:, :1].values
+knnDummyTestFeatures = knnDummyTestData.iloc[:, 1:].values
 
 # knnTrainLabels = knnTrainData.iloc[:, :1].values
 # knnTrainFeatures = knnTrainData.iloc[:, 1:].values 
 
 # knnTestLabels = knnTestData.iloc[:, :1].values
 # knnTestFeatures = knnTestData.iloc[:, 1:].values
-
 
 
 def distanceBetweenPoints(pointOneArray, pointTwoArray):
@@ -39,7 +41,7 @@ def distanceBetweenPoints(pointOneArray, pointTwoArray):
 
 def findNearestIndex(point, points, ignoreIndices):
     bestIndex = 0
-    while(bestIndex in ignoreIndices):
+    while(bestIndex in ignoreIndices): # ensure the initialization isnt an ignored index.
         bestIndex += 1
     index = 0
     for p in points:
@@ -58,4 +60,19 @@ def findKNearest(k, point, points):
         bestIndices.extend([nearestPointIndex])
     return bestIndices
 
-print(findKNearest(2, tryA, tryB))
+def vote(k, point, points, labels):
+    totalVote = 0
+    kBestIndicesArray = findKNearest(k, point, points)
+    for index in kBestIndicesArray:
+        thisVote = labels[index]
+        totalVote += thisVote
+    if totalVote < 0:
+        return -1
+    elif totalVote > 0:
+        return 1
+    else:
+        print('Tie in vote occured!')
+        return 0 # tie situation
+
+# print(vote(5, knnDummyTestFeatures[0], knnDummyTrainFeatures, knnDummyTrainLabels))
+print(knnDummyTestLabels[0][0])
