42db54a63063e95ed82b5e0fcbe4cb83a2908c35
Tarren Engberg <tarren@outlook.com>
2019-05-23 15:13:29 -0700

    Need to rewrite makeCentroids to work with vectors.

--- project4/p4/kmeans.py
+++ project4/p4/kmeans.py
@@ -3,20 +3,45 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#initialization
-tdf = pd.read_csv('p4-data.txt', header=None, nrows=2)
-df = pd.read_csv('p4-data.txt', header=None)
 
-np.random.seed(200)
+### utilities ###
+np.random.seed(200) # maybe make this based on time or something ..
 
-k = 3
+def numRows(df):
+    return len(df)
+
+def numCols(df):
+    return len(df.columns)
+
+def getRand(cap):
+    return np.random.randint(0,cap)
+
+def makeCentroids(k): # make k vectors, each w/ 
+    arr = np.zeros(k)
+    for i in range(arr.size):
+        arr[i] = getRand(255)
+    return arr
+
+
+### get data ###
+dfSample = pd.read_csv('test-data.txt', dtype='int', delimiter = ',', header=None)
+# dfFirst2Rows = pd.read_csv('p4-data.txt', header=None, nrows=2)
+# df = pd.read_csv('p4-data.txt', header=None)
+
+
+### get parameter(s) ###
+k = 3 # default
 if len(sys.argv) > 1:
-    k = float(sys.argv[1])
-
-print(tdf)
+    k = int(sys.argv[1]) # if user supplies parameter
 
 
+### initialization ###
 
-# centroids = {
+# make a k long list of random centroids (values between 0 and 255)
+sampleCentroids = makeCentroids(3)
+centroids = makeCentroids(k)
 
-# }
+
+
+
+# print(  )
