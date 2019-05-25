e0bb7fbeb7aefbacfa08f00dd62ba1f8cf499800
Tarren Engberg <tarren@outlook.com>
2019-05-25 14:49:04 -0700

    start over, better understanding of the problem.

---
+++ project4/p4/kmeans-old.py
@@ -0,0 +1,53 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+### utilities ###
+np.random.seed(200) # maybe make this based on time or something ..
+
+def numRows(df):
+    return len(df)
+
+def numCols(df):
+    return len(df.columns)
+
+def getRand(cap):
+    return np.random.randint(0,cap)
+
+def makeRandomVector(length, max):
+    vector = np.zeros(length)
+    for v in range(vector.size):
+        vector[v] = getRand(max)
+    return vector
+
+def makeCentroids(k, df): # make k vectors, each w/ col random values between 0 and 255
+    centroids = {}
+    for i in range(k):
+        centroids[i] = makeRandomVector(numCols(df), 255) # columns are dimensions
+    return centroids
+
+### get data ###
+dfSample = pd.read_csv('test-data.txt', dtype='int', delimiter = ',', header=None)
+dfFirst2Rows = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None, nrows=2)
+df = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None)
+
+
+### get parameter(s) ###
+k = 3 # default
+if len(sys.argv) > 1:
+    k = int(sys.argv[1]) # if user supplies parameter
+
+
+### initialization ###
+sampleCentroids = makeCentroids(3, dfSample)
+# centroids = makeCentroids(k, df)
+
+
+### assignment ###
+def assignment(df, centroids):
+    for k in centroids.keys(): # k times
+        df
+
+# print(sampleCentroids.keys())
--- project4/p4/kmeans.py
+++ project4/p4/kmeans.py
@@ -16,17 +16,18 @@ def numCols(df):
 def getRand(cap):
     return np.random.randint(0,cap)
 
-def makeCentroids(k): # make k vectors, each w/ 
-    arr = np.zeros(k)
-    for i in range(arr.size):
-        arr[i] = getRand(255)
-    return arr
+def dfRowToVector(df, rowIdx):
+    vector = np.zeros(numCols(df))
+    for i in range(vector.size):
+        vector[i] = df.iloc[rowIdx][i]
+    return vector
+
 
 
 ### get data ###
 dfSample = pd.read_csv('test-data.txt', dtype='int', delimiter = ',', header=None)
-# dfFirst2Rows = pd.read_csv('p4-data.txt', header=None, nrows=2)
-# df = pd.read_csv('p4-data.txt', header=None)
+dfFirst2Rows = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None, nrows=2)
+# df = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None)
 
 
 ### get parameter(s) ###
@@ -35,13 +36,29 @@ if len(sys.argv) > 1:
     k = int(sys.argv[1]) # if user supplies parameter
 
 
-### initialization ###
+### main ###
 
-# make a k long list of random centroids (values between 0 and 255)
-sampleCentroids = makeCentroids(3)
-centroids = makeCentroids(k)
+# pick k seeds
+# def getKSeeds(k, df):
+#     seeds = {}
+#     for i in range(k):
+#         randIdx = getRand(numRows(df))
+#         seeds[i] = df[randIdx]
+#     return seeds
 
 
 
+# assign all points to its nearest seed
 
-# print(  )
+# compute k centroids
+
+# reassign all points to its nearest centroid
+
+# def assignment(df, centroids):
+#     for k in centroids.keys(): # k times
+#         df
+
+
+# print(getKSeeds(1, dfSample))
+# print( dfRowToVector(dfSample, 0) )
+# print(dfSample.iloc[0])
