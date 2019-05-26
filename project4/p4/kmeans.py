bef90b92c205be90071bae49246ce5536a00fd76
Tarren Engberg <tarren@outlook.com>
2019-05-25 17:12:14 -0700

    can find closest centroid to a vector

--- project4/p4/kmeans.py
+++ project4/p4/kmeans.py
@@ -1,11 +1,12 @@
 import sys
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 ### utilities ###
-np.random.seed(200) # maybe make this based on time or something ..
+np.random.seed(200) # maybe make this based on time or something..
 
 def numRows(df):
     return len(df)
@@ -22,11 +23,17 @@ def dfRowToVector(df, rowIdx):
         vector[i] = df.iloc[rowIdx][i]
     return vector
 
+def getKSeeds(k, df):
+    seeds = {}
+    for i in range(k):
+        randIdx = getRand(numRows(df))
+        seeds[i] = dfRowToVector(df, randIdx)
+    return seeds
 
 
 ### get data ###
 dfSample = pd.read_csv('test-data.txt', dtype='int', delimiter = ',', header=None)
-dfFirst2Rows = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None, nrows=2)
+dfF2 = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None, nrows=2)
 # df = pd.read_csv('p4-data.txt', dtype='int', delimiter = ',', header=None)
 
 
@@ -36,29 +43,61 @@ if len(sys.argv) > 1:
     k = int(sys.argv[1]) # if user supplies parameter
 
 
-### main ###
-
-# pick k seeds
-# def getKSeeds(k, df):
-#     seeds = {}
-#     for i in range(k):
-#         randIdx = getRand(numRows(df))
-#         seeds[i] = df[randIdx]
-#     return seeds
+### pick k seeds ###
+seedsSample = getKSeeds(2, dfSample)
+seedsF2 = getKSeeds(k, dfF2)
+# seeds = getKSeeds(k, df)
 
 
+### assign all points to its nearest seed ###
 
-# assign all points to its nearest seed
+def eucDist(vector): # distance from the origin 0
+    sum = 0
+    for i in range(vector.size):
+        vs = vector[i] * vector[i]
+        sum = sum + vs
+    return math.sqrt(sum)
 
-# compute k centroids
+def getDistanceBetween(vectorA, vectorB):
+    return math.fabs( eucDist(vectorA) - eucDist(vectorB) )
 
-# reassign all points to its nearest centroid
+def findClosestCentroid(rowVector, centroids):
+    smallestDist = float("inf")
+    bestCentroidInd = 0
+    for centroid in centroids:
+        currDist = getDistanceBetween(centroids[centroid], rowVector)
+        if currDist < smallestDist:
+            smallestDist = currDist
+            bestCentroidInd = centroid
+    return bestCentroidInd
 
-# def assignment(df, centroids):
-#     for k in centroids.keys(): # k times
-#         df
+# def assign(df, centroids):
+#     assignment = np.zeros(numRows(df)) # a rows long list of which each index is the k orientation.
+#     for rowIdx in range(numRows(df)):
+#         assignment[rowIdx] = findClosestCentroid(dfRowToVector(df, rowIdx), centroids)
+#     return assignment
+
+
+#### compute k centroids ###
+
+### reassign all points to its nearest centroid ###
+
+# changeCheck should take the old and new assignments and return the amount of different indecies.
+
 
 
 # print(getKSeeds(1, dfSample))
-# print( dfRowToVector(dfSample, 0) )
-# print(dfSample.iloc[0])
+# assign(dfSample, seedsSample)
+# print( assign(dfSample, seedsSample) )
+
+
+# print("Sample -> ", seedsSample)
+
+testVec = np.array([0,2,3])
+testtCentroids = { 0: np.array([0,1,2]), 1: np.array([1,2,3]), 2: np.array([7,8,9]) }
+
+# print("vector -> : ",  testVec)
+# print("centroids -> : ", testtCentroids)
+# print("closest ind -> : ", findClosestCentroid(testVec, testtCentroids))
+
+# print(  getDistanceBetween( np.array([1,3]), np.array([4,4]) )  )
